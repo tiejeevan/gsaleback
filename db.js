dd56f9958e6d97e04c2d92c182710f176a1f65ef
Jeevan kumar Chowdapu <jeevankumarchowdapu@Jeevans-MacBook-Pro.local>
2025-10-28 18:49:17 -0400

    Initial gsale commit

---
+++ db.js
@@ -0,0 +1,11 @@
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false // Required for Render Postgres
+    }
+});
+
+module.exports = pool;
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/api/auth', require('./routes/auth'));
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
